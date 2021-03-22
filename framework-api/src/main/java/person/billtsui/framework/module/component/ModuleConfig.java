4509e1509386f2a6c2ec3951d7daee1c720db135
Bill Tsui <billkuentsui@outlook.com>
2021-03-22 16:21:18 +0800

    填充配置器对象

---
+++ framework-api/src/main/java/person/billtsui/framework/module/component/ModuleConfig.java
@@ -0,0 +1,11 @@
+package person.billtsui.framework.module.component;
+
+import java.util.Map;
+
+/**
+ * @author billtsui
+ * @date 2021/3/22
+ */
+public class ModuleConfig {
+    private Map<String, ProviderConfig> providers;
+}
